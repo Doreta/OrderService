06756ab5e446d3d811027dced4d95c0ad479e65f
doretamaliqi <doreta.maliqi@prosperoware.com>
2021-05-23 00:30:40 +0200

    Adding something in deploy file2.0

--- services/deploy.sh
+++ services/deploy.sh
@@ -1,2 +1,5 @@
 #! /bin/bash
-echo "HELLO THERE"
+echo $env
+echo $CODEBUILD_SRC_DIR
+npm install -g serverless
+serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/target/$env -v -r eu-central-1
