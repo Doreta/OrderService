ff26115a4b98660ec90f9832aa8473191463e161
doretamaliqi <doreta.maliqi@prosperoware.com>
2021-05-23 03:09:21 +0200

    commit

--- services/deploy.sh
+++ services/deploy.sh
@@ -1,8 +1,4 @@
 #! /bin/bash
 
 npm install -g serverless
-echo 'here'
-pwd
-ls
-npm install --silent --no-progress
 serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/target/$env -v -r eu-west-1
