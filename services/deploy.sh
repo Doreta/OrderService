6c6d9ef4972e67f28022bf9a41b8c6a21a340dbb
doretamaliqi <doreta.maliqi@prosperoware.com>
2021-05-23 02:48:48 +0200

    e heka cd

--- services/deploy.sh
+++ services/deploy.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
 npm install -g serverless
+echo 'here'
+pwd
 npm install --silent --no-progress
 serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/target/$env -v -r eu-west-1
