7aa0a245b2b3ffef535b207feee758ff6c928d64
doretamaliqi <doreta.maliqi@prosperoware.com>
2021-05-24 10:45:33 +0200

    Some changes

--- services/deploy.sh
+++ services/deploy.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
 npm install -g serverless
-cd target/$env
 npm install
-cd services && serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/target/$env -v -r eu-west-1
+cd target/$env && serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/target/$env -v -r eu-west-1
