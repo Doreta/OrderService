04db9d66c9711a69b523ed67ec9f0db076e103dc
doretamaliqi <doreta.maliqi@prosperoware.com>
2021-05-25 01:18:01 +0200

    deploy.sh1

--- services/deploy.sh
+++ services/deploy.sh
@@ -3,6 +3,6 @@
 
 mkdir -p .serverless
 cp -r $CODEBUILD_SRC_DIR/target/$env/* $PWD/.serverless
-serverless deploy --stage $env --package /.serverless -v -r eu-west-1
+serverless deploy --stage $env --package .serverless -v -r eu-west-1
 
 
